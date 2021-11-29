36e3150035e9b2b1473dff4c82734e1adc2a31fe
its_d <dineshparadesi94@gmail.com>
2021-11-29 22:14:25 +1000

    issue#1 system config and site config files

---
+++ config/Config.php
@@ -0,0 +1,3 @@
+<?php
+const BASE_URL = "http://localhost:8082";
+const ASSETS_URL = BASE_URL . '/assets';
---
+++ system/Config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ROOT_PATH', dirname(__DIR__));
+define('VIEWS_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views');
