720ca9acddc29e3e0f1f7e79d463bfeae617b1f9
its_d <dineshparadesi94@gmail.com>
2021-11-30 23:40:17 +1000

    issue#1 Added stock data migration

---
+++ migrations/m0001_stock_data.php
@@ -0,0 +1,41 @@
+<?php
+
+
+use App\System\Application;
+use App\System\Database;
+
+class m0001_stock_data
+{
+
+    /**
+     * Build
+     */
+    public function up()
+    {
+        $SQL = "CREATE TABLE stock_data (
+                id_no INT AUTO_INCREMENT PRIMARY KEY,
+                date DATE NOT NULL,
+                stock_name VARCHAR(255) NOT NULL,
+                price VARCHAR(255) NOT NULL
+            )  ENGINE=INNODB;";
+        $this->db()->pdo->exec($SQL);
+    }
+
+    /**
+     * Tear down
+     */
+    public  function down()
+    {
+        $SQL = "DROP TABLE stock_data IF EXISTS";
+        $this->db()->pdo->exec($SQL);
+    }
+
+    /**
+     * Database instance
+     * @return Database
+     */
+    protected function db(): Database
+    {
+        return Application::$app->db;
+    }
+}
