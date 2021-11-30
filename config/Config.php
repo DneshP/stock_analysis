30495d6e3b16a2756e6d7708ed1507dde24ef283
its_d <dineshparadesi94@gmail.com>
2021-11-30 23:41:03 +1000

    issue#1 defined constants for application

--- config/Config.php
+++ config/Config.php
@@ -1,3 +1,9 @@
 <?php
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 const BASE_URL = "http://localhost:8082";
 const ASSETS_URL = BASE_URL . '/assets';
+define("DB_USER", $_ENV['DB_USER']);
+define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
+define("DB_DSN", $_ENV['DB_DSN']);
--- system/Config.php
+++ system/Config.php
@@ -2,3 +2,5 @@
 
 define('ROOT_PATH', dirname(__DIR__));
 define('VIEWS_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views');
+define('RUNTIME_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'runtime');
+define('MIGRATION_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'migrations');
