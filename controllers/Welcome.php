18e3cde15c3e9e0c25141eff7b0c5fdd8c87c91e
its_d <dineshparadesi94@gmail.com>
2021-12-01 22:51:01 +1000

    issue#2 added functionality ->upload stream into database, added date validation

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\System\Controller;
+use App\System\Request;
+use App\Models\Welcome as WelcomeModel;
 
 /**
  * Class Welcome
@@ -11,6 +13,18 @@ use App\System\Controller;
 class Welcome extends Controller
 {
     private $header = 'layouts/header';
+
+    /** @var WelcomeModel */
+    private $model;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(Request $request)
+    {
+        parent::__construct($request);
+        $this->model = new WelcomeModel();
+    }
 
     /**
      * @return string
@@ -37,4 +51,95 @@ class Welcome extends Controller
 
         return $this->render($viewData);
     }
+
+    /**
+     * Uploads the received chunk
+     * @return false|string|void
+     */
+    public function streamStockData()
+    {
+        $stream = json_decode(file_get_contents('php://input'));
+        if ($stream->status === 'on') {
+            $data = explode( ';base64,', $stream->chunk);
+            if (isset($data[1])) {
+                $base64Decoded = base64_decode($data[1]);
+                $rawData = explode(PHP_EOL, $base64Decoded);
+                if ($stream->hasTags) {
+                    $tags = explode(',', $rawData[0]);
+                    array_shift($tags);
+                    $diff = array_diff($tags, $this->model->columns());
+                    if(!empty($diff)) {
+                        return json_encode(['status' => false, 'data' => 'Invalid data provided, 
+                        Please check column headings', 'debug' => json_encode($diff)]);
+                    }
+                    array_shift($rawData);
+                }
+                $data = [];
+
+                foreach ($rawData as &$r) {
+                        $raw = explode(',', $r);
+                            array_shift($raw);
+                        if (!empty($raw)) {
+                            $raw[0] = $this->formatDate($raw[0]);
+                            for ($i = 0; $i < count($this->model->columns()); $i++ ) {
+                                $value = $raw[$i] ?? null;
+                                $raw[$i] = "'$value'";
+                            }
+                            $stock = implode(',', $raw);
+                            $data [] = "(".$stock.")";
+                         }
+                }
+                 $status = $this->model->insertStockData(implode(',', $data));
+                 $message = $status ? 'Received Chunk' : 'Error Receiving Chunk. Internal server error';
+            } else {
+                $status = true;
+                $message = 'Empty Chunk';
+            }
+            return json_encode(['status' => $status, 'data' => $message]);
+        }
+        if ($stream->status === 'done') {
+               $data = $this->model->getStockNames();
+               $status = $data ?: false;
+           return json_encode(['status' => $status, 'data' => json_encode($data)]);
+        }
+    }
+
+    /**
+     * Converts date to YYYY-mm-dd
+     * if date matches dd-mm-yyyy and mm-dd-yyyy
+     * dd-mm-yyyy takes precedence
+     * any other format are replaced to 0000-00-00
+     * supported date formats
+     * YYYY-mm-dd
+     * dd-mm-YYYY
+     * mm-dd-YYYY
+     * @param $dateString
+     * @return false|string|null
+     */
+    private function formatDate($dateString)
+    {
+        $dateString = str_replace('/', '-', $dateString);
+        $monthRegex = '(0[1-9]|1[0-2])';
+        $yearRegex = '\d{4}';
+        $dayRegex = '(0[1-9]|[12][0-9]|3[01])';
+        $yearMonthDay = "/^$yearRegex-$monthRegex-$dayRegex$/";
+        $dayMonthYear = "/^$dayRegex-$monthRegex-$yearRegex$/";
+        $monthDayYear = "/^$monthRegex-$dayRegex-$yearRegex$/";
+        $formats = [$dayMonthYear, $monthDayYear, $yearMonthDay];
+        $formattedDate = '0000-00-00';
+
+        foreach ($formats as $format) {
+            $result = [];
+            preg_match($format, $dateString, $result);
+            if (!empty($result)) {
+                $date = date_create($dateString);
+                if ($date) {
+                    $formattedDate = date_format($date, "Y-m-d");
+                    break;
+                }
+            }
+        }
+        return $formattedDate;
+    }
+
 }
