5ad1a3621a40bc66ea4eba1a93c252eaabdf808f
its_d <dineshparadesi94@gmail.com>
2021-12-03 03:01:58 +1000

    issue#1 Added validation for model

--- system/Model.php
+++ system/Model.php
@@ -23,31 +23,49 @@ abstract class Model
 
     abstract public function columns();
 
-    public function fetch($params)
+    public function fetch($filter, $mode = PDO::FETCH_OBJ)
     {
         $select = '*';
         $where = '';
-        if (!empty($params->select)) {
-            $select = implode(',', $params->select);
+        $orderBy = '';
+        if (!empty($filter->select)) {
+            $select = implode(',', $filter->select);
         }
-        if (!empty($params->where)) {
-            $count = count($params->where) - 1;
-            foreach ($params->where as $whereKey => $column) {
+        if (!empty($filter->where)) {
+            $count = count($filter->where) - 1;
+            foreach ($filter->where as $whereKey => $column) {
                 $key = array_keys($column)[0];
-                $where .= $key . '=' . $column[$key];
+                $data = $column[$key];
+                $operation = $data[0];
+                $value = $data[1];
+                $where .= " $key $operation '" . $value . "' ";
                 if ($whereKey < $count) {
-                    $where .= 'AND';
+                    $where .= ' AND ';
                 }
             }
         } else {
             $where = 'TRUE';
         }
+        if (!empty($filter->orderBy)) {
+            $count = count($filter->orderBy) - 1;
+            foreach ($filter->orderBy as $key => $condition) {
+                $orderBy .= " $condition ";
+                if ($key < $count) {
+                    $orderBy .= ' AND ';
+                }
+            }
+        }
 
         $tableName = $this->tableName();
-        $SQL = "SELECT $select FROM $tableName WHERE $where";
-        $statement = $this->prepare($SQL);
-        $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_COLUMN);
+        $SQL = "SELECT $select FROM $tableName WHERE $where $orderBy ";
+        try {
+            $statement = $this->prepare($SQL);
+            $statement->execute();
+            return $statement->fetchAll($mode);
+        } catch (\Exception $exception) {
+            $this->log($exception);
+            return false;
+        }
     }
 
     /**
@@ -69,14 +87,38 @@ abstract class Model
     }
 
     /**
-     * Select = [column1, column2]
-     * Where = [[key => value], [key => value]]
+     * Truncate table
      */
-    protected function fetchParams(): stdClass
+    public function fresh()
+    {
+        $tableName = $this->tableName();
+        $SQL = "TRUNCATE TABLE $tableName";
+        $this->db->pdo->exec($SQL);
+    }
+
+    /**
+     * Select = [column1, column2]
+     * Where = [[key => [operation, value, unquote]], [key => [operation,value, unquote]]
+     * where condition applied as where key >= 'value'; by default
+     * if unquote false where key >= value
+     */
+    protected function filterParams(): stdClass
     {
         $params = new stdClass();
         $params->select = [];
         $params->where = [];
+        $params->orderBy = [];
         return $params;
+    }
+
+    /**
+     * Log Errors
+     * @param $message
+     */
+    protected function log($message)
+    {
+        $error_log = RUNTIME_PATH . DIRECTORY_SEPARATOR . 'log.txt';
+        $log = '['.date('Y-m-d H:i:s').']' . '-' . $message . PHP_EOL;
+        file_put_contents($error_log, $log, FILE_APPEND);
     }
 }
