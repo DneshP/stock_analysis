993a627a1fa63cf62c06c7587f42dee76fb05a6f
its_d <dineshparadesi94@gmail.com>
2021-12-06 11:00:56 +1000

    issue#12 update paths to config

--- src/migrations.php
+++ src/migrations.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR;
-require_once __DIR__ . DIRECTORY_SEPARATOR;
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Config/Config.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'System/Config.php';
 
 use App\System\Application;
 
